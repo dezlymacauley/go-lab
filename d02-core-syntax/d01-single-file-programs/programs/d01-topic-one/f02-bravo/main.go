1bd06712f6fc447a8b9d2c0bc8657e6b73d60a3f
dezlymacauley <dezlymacauley@gmail.com>
2026-09-14 11:56:42 +0200

    implemented single-file workspace

---
+++ d02-core-syntax/d01-single-file-programs/programs/d01-topic-one/f02-bravo/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println()
+	fmt.Println("This is f02-bravo.go")
+    fmt.Println()
+}
---
+++ d02-core-syntax/d01-single-file-programs/programs/d02-topic-two/f01-charlie/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println()
+	fmt.Println("This is f01-charlie.go")
+    fmt.Println()
+}
